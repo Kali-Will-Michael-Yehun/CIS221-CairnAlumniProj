70e23a72fc24c68e299eaba1e62c4bec1b52111c
Mike15James <56136472+Mike15James@users.noreply.github.com>
2021-03-20 20:46:42 -0400

    Update GroupProject.sql

--- GroupProject.sql
+++ GroupProject.sql
@@ -4,18 +4,23 @@ CREATE TABLE Alum (
   AlumID INT NOT NULL AUTO_INCREMENT,
   FirstName VARCHAR(255) NOT NULL,
   LastName VARCHAR(255) NOT NULL,
-  GradYear INT NOT NULL,
+  Phone INT(10) UNSIGNED ZEROFILL NOT NULL,
+  Address VARCHAR(255) NOT NULL,
+  Email VARCHAR(255) NOT NULL,
+  LinkedIn VARCHAR(255) NOT NULL,
   PRIMARY KEY (AlumID)
 );
 CREATE TABLE Career (
   AlumID INT NOT NULL AUTO_INCREMENT,
-  Name VARCHAR(255) NOT NULL,
   Industry VARCHAR(255) NOT NULL,
   Company VARCHAR(255) NOT NULL,
   Title VARCHAR(255) NOT NULL,
   Year_Started INT NOT NULL,
   Year_Ended INT NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (AlumID) REFERENCES Alum(AlumID),
-  FOREIGN KEY (Name) REFERENCES Alum(FIrstName)
+  Location VARCHAR(255) NOT NULL,
+  PRIMARY KEY (AlumID),
+  FOREIGN KEY (AlumID) REFERENCES Alum(AlumID)
 );
+ALTER TABLE Career
+  CONSTRAINT validIndustry check (Industry in ('Medicine', 'Finance', 'Education', 'Technology', 'Law', 'Social Services', 'Counseling', 'Business', 'Agriculture', 'Manufacturing', 'Operations', 
+                                               'Healthcare', 'Sales', 'Marketing', 'Administration', 'Information Technology', 'Support', 'Arts', 'Design', 'Media', 'Communication', 'Human Resources','engineering'))
