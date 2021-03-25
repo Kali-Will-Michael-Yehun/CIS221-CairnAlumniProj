fb17260e29870fcbbe0e1780e9a3942ca0e9bf69
yk084 <68211358+yk084@users.noreply.github.com>
2021-03-25 00:50:21 -0400

    Create Yehun_AlumInsertStatements.sql

---
+++ Yehun_AlumInsertStatements.sql
@@ -0,0 +1,7 @@
+INSERT INTO Alum (FirstName, LastName, Phone, Address, Email, LinkedIn, Hiring)
+VALUES 
+	('Samuel', 'Gabriels', 2159068338, 'Greater Philadelphia', 'samueljgabriels@gmail.com', 'linkedin.com/in/samuel-gabriels-027047181', 0)
+	('Miguel', 'Tapia', 6469723880, 'Greater Philadelphia', 'mtapia@cairn.edu', 'linkedin.com/in/mtapiafdez', 0)
+	('Kadin', 'Tran', 4089723565, 'San Jose, California', 'ktran@cairn.edu', 'linkedin.com/in/kadintran', 0)
+	('Kelly', 'Harris', '', 'Langhorne, Pennsylvania', 'kharris@cairn.edu', 'linkedin.com/in/kelly-harris-0b2b8490', 0)
+	('Timothy', 'Smith', 2157024863, 'Thousand Oaks, California', 'tsmith@cairn.edu', 'linkedin.com/in/timothy-smith-539a71180', 0)
