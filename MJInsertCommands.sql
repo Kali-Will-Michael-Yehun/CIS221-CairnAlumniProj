ac26797034986bf2bc4e1dcb539e471cb9a30572
Mike15James <56136472+Mike15James@users.noreply.github.com>
2021-03-25 03:33:46 -0400

    Add files via upload

---
+++ MJInsertCommands.sql
@@ -0,0 +1,14 @@
+INSERT INTO Alum(FirstName, LastName, LinkedIn)
+VALUES ('Tyler', 'Carrier', 'Tyler Carrier');
+
+INSERT INTO Alum(FirstName, LastName, Phone, Address, Email)
+VALUES ('Miguel', 'Tapia', 6469723880, '200 Manor Avenue, Langhorne PA', 'mtapiafdez@gmail.com');
+
+INSERT INTO Alum(FirstName, LastName, Email)
+VALUES ('Andrea', 'Stange', 'astange@cairn.edu');
+
+INSERT INTO Alum(FirstName, LastName, LinkedIn)
+VALUES ('Ryan', 'Grande', 'Ryan Del Grande');
+
+INSERT INTO Alum(FirstName, LastName, LinkedIn)
+VALUES ('Tyler', 'Smith', 'Tyler Smith');
