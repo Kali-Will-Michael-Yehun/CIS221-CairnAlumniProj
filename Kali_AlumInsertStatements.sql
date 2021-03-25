6f911c1280eea15a13746b67e178f7d1bf6eb87b
kjburres <80986181+kjburres@users.noreply.github.com>
2021-03-24 22:44:06 -0400

    Alumni Insert Statements!
    These are my Alum Insert Statements :)

---
+++ Kali_AlumInsertStatements.sql
@@ -0,0 +1,17 @@
+
+--Tieagra
+INSERT INTO Alum(FirstName, LastName, Phone, Address, Email, LinkedIn)
+Values ('Tieagra', 'McNiel', 7178752156, 'Lancaster, PA', 'tieagramcniel@eurofinsUS.com', 'https://www.linkedin.com/in/tieagra-mcneil/detail/contact-info/');	
+
+--Julian
+INSERT INTO Alum(FirstName, LastName, Phone, Email, LinkedIn)
+Values ('Julian', 'Collazo', 176669055, 'jcollazo677@yahoo.com', 'https://www.linkedin.com/in/julian-collazo-285343116/detail/contact-info/');
+
+
+--Brie, Rea, and Liz
+INSERT INTO Alum(FirstName, LastName, Email, LinkedIn)
+Values
+('Brianna', 'Norton', 'bn086@cairn.edu', 'linkedin.com/in/brianna-norton')
+,('Rea', 'Fearson', 'rea.a.fearson@gmail.com', 'linkedin.com/in/reafearson')
+,('Elizabeth', 'Decker', 'ew479@cairn.edu', 'linkedin.com/in/elizabeth-decker-6109a8138');
+	
