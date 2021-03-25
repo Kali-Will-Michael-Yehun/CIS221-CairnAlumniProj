6607cebdc450b80421c4aa387cf18acb0ffdd0c6
kjburres <80986181+kjburres@users.noreply.github.com>
2021-03-24 22:22:51 -0400

    Adding Tables
    Added Kali and Will's CREATE TABLE statements to the document!

--- GroupProject.sql
+++ GroupProject.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE AlumniTracker;
 USE AlumniTracker;
 
+--Michael
 CREATE TABLE Alum (
   AlumID INT NOT NULL AUTO_INCREMENT,
   FirstName VARCHAR(255) NOT NULL,
@@ -11,6 +12,23 @@ CREATE TABLE Alum (
   LinkedIn VARCHAR(255) NOT NULL,
   PRIMARY KEY (AlumID)
 );
+
+--Will
+CREATE TABLE IF NOT EXISTS Degrees (
+	DegreeID int not null auto_increment primary key 
+  ,FOREIGN KEY (AlumID) REFERENCES Alum (AlumID)
+	,Und/Master char(1)
+	,DegreeLevel varchar(255) not null
+  ,School varchar(255) not null
+  ,GradDate DATE not null
+  ,InternshipCompany varchar(255)
+  ,InternshipPosition varchar(255)
+  ,IntershipDuration int
+);
+-- Dual-Level degrees can be found by determining which students received a BS in Business and MBA from Cairn --
+-- IntershipDuration: measured in terms of weeks --
+
+--Michael
 CREATE TABLE Career (
   AlumID INT NOT NULL AUTO_INCREMENT,
   Industry VARCHAR(255) NOT NULL,
@@ -22,6 +40,8 @@ CREATE TABLE Career (
   PRIMARY KEY (AlumID),
   FOREIGN KEY (AlumID) REFERENCES Alum(AlumID)
 );
+
+--Yehun
 CREATE TABLE GuestSpeakers (
 	SpeechID INT NOT NULL,
 	Speech_Date DATE,
@@ -32,6 +52,15 @@ CREATE TABLE GuestSpeakers (
 	PRIMARY KEY (SpeechID), 
 	FOREIGN KEY (AlumID) REFERENCES Alum(AlumID)
 );
+
+--Kali
+CREATE TABLE IF NOT EXISTS Comments(
+	AlumID int not null Primary Key
+	,OtherDetails text 
+	,Foreign Key(AlumID)REFERENCES Alum(AlumID)
+	,UNIQUE(AlumID)		--https://www.w3schools.com/sql/sql_unique.asp
+)Engine = InnoDB;
+
 ALTER TABLE Career 
   CONSTRAINT validIndustry check (Industry in ('Medicine', 'Finance', 'Education', 'Technology', 'Law', 'Social Services', 'Counseling', 'Business', 'Agriculture', 'Manufacturing', 'Operations', 
-                                               'Healthcare', 'Sales', 'Marketing', 'Administration', 'Information Technology', 'Support', 'Arts', 'Design', 'Media', 'Communication', 'Human Resources','engineering'))
+                                               'Healthcare', 'Research', 'Sales', 'Marketing', 'Administration', 'Information Technology', 'Support', 'Arts', 'Design', 'Media', 'Communication', 'Human Resources','Engineering'))
